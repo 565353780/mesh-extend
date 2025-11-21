246579e2fd11507efddda7fa6ea92dfad996e0c0
chli <chli@chlideMacBook-Pro.local>
2025-11-21 16:57:44 +0800

    add setup bash files

---
+++ dev_setup.sh
@@ -0,0 +1,7 @@
+cd ..
+git clone git@github.com:565353780/wn-nc.git
+
+cd wn-nc
+./dev_setup.sh
+
+pip install open3d
---
+++ setup.sh
@@ -0,0 +1,7 @@
+cd ..
+git clone https://github.com/565353780/wn-nc.git
+
+cd wn-nc
+./setup.sh
+
+pip install open3d
